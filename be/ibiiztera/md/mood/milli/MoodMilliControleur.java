9422326201c22a1ac8b1b103808963d373373b98
DAHMEN Manuel <manuel.dahmen@gmail.com>
2012-04-21 17:21:01 +0200

    changed: implementation ++

--- be/ibiiztera/md/mood/milli/MoodMilliControleur.java
+++ be/ibiiztera/md/mood/milli/MoodMilliControleur.java
@@ -13,11 +13,11 @@ import java.awt.image.BufferedImage;
  */
 public interface MoodMilliControleur {
     public void resolution(int x, int y);
-    public void grid(int pppx, int pppy);
+    public void grid(int pixelx, int pixely);
     public void showGrid(boolean show);
     public void ligne(Point p1, Point p2);
-    public void point(Point p);
-    public void bezier(Point p1, Point p2, Point p3, Point p4);
+    public void point(Point p, ColorO co);
+    public void bezier(Point p1, Point p2, Point p3, Point p4, ColorO co);
     public BufferedImage image();
     
 }
