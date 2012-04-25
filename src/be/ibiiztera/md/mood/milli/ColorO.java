118c68638f9c7161fc46595ecf2e561117918b77
Manuel DAHMEN <manuel.dahmen@gmail.com>
2012-04-25 09:31:32 +0200

    compile

---
+++ src/be/ibiiztera/md/mood/milli/ColorO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ibiiztera.md.mood.milli;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Manuel DAHMEN
+ */
+public class ColorO {
+    public Color color;
+}
